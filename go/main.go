9c0f97cb27c3013599da699d452c868103b11d8c
sor4chi <sorakawa0218@gmail.com>
2023-11-24 17:22:38 +0900

    envを引数で

--- go/main.go
+++ go/main.go
@@ -781,8 +781,7 @@ func initialize(c echo.Context) error {
 			}
 			defer dbx.Close()
 
-			os.Setenv("ISUCON_DB_HOST", string(db))
-			out, err := exec.Command("/bin/sh", "-c", SQLDirectory+"init.sh").CombinedOutput()
+			out, err := exec.Command("/bin/sh", "-c", SQLDirectory+"init.sh", string(db)).CombinedOutput()
 			if err != nil {
 				c.Logger().Errorf("Failed to initialize %s: %v", string(out), err)
 				return
