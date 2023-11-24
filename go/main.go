aeb8f191bc9c6f7e2666bfe9d30497756225cf0d
sor4chi <sorakawa0218@gmail.com>
2023-11-24 18:53:11 +0900

    god bye logger

--- go/main.go
+++ go/main.go
@@ -61,7 +61,6 @@ func main() {
 	time.Local = time.FixedZone("Local", 9*60*60)
 
 	e := echo.New()
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
